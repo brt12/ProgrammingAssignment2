672ba43987000e078644ab2e3d351fe4968083cd
brt12 <biteshtiwari@gmail.com>
2018-08-12 22:46:10 +0530

    week2 prog assignment
    completed the week 2 programming assignment of finding the Inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## This is a test
+## Following function helps to resuce the computaion time in calculating Inverse of a matrix by reading in cache
 
-## Write a short comment describing this function
+
+## The fllowing matrix cache the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+    m<- NULL
+    set<-function(y){
+          x<<-y
+          m<<-NULL
+    }
+    get<-function()x
+    setinverse_matrix<-function(inv_matrix) m<<-inv_matrix
+    getinverse_matrix<-function()m
+    list(set=set,get=get,setinverse_matrix=setinverse_matrix,getinverse_matrix=getinverse_matrix)
 
 }
 
 
-## Write a short comment describing this function
 
+##This calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinverse_matrix function
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        m<-x$getinverse_matrix()## Return a matrix that is the inverse of 'x'
+        if(!is.null(m)){
+          message("getting cached data")
+          return(m)
+        }
+        data<-x$get()
+        m<-solve(data)
+        x$setinverse_matrix(m)
+        m
+        }
